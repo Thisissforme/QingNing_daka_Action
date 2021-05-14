f662472456e94958938752d9b4ac5153e70df74d
CallMeChou <2079986882@qq.com>
2021-05-14 23:24:31 +0800

    Create tujian.py

---
+++ tujian.py
@@ -0,0 +1,20 @@
+import base64
+import json
+import requests
+import time
+img_path="qrcode_temp.png"
+def base64_api(uname='Zion', pwd='123456qwerty', img=img_path):
+    with open(img, 'rb') as f:
+        base64_data = base64.b64encode(f.read())
+        b64 = base64_data.decode()
+    data = {"username": uname, "password": pwd, "image": b64}
+    result = json.loads(requests.post("http://api.ttshitu.com/base64", json=data).text)
+    if result['success']:
+        return result["data"]["result"]
+    else:
+        return result["message"]
+
+
+if __name__ == "__main__":
+    result = base64_api()
+    print(result)
