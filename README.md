a31ad69af36d86b4b41674438737de283da3e129
CallMeChou <2079986882@qq.com>
2021-05-15 19:35:19 +0800

    Update tujian.py

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # QingNing_daka_Action
+<<<<<<< HEAD
 每天qing_ning daka 的github action 版本测试   
+=======
+每天打卡的github action 版本测试    
+>>>>>>> 15cf41330ecc12a9f3b213c771cebbe382b980c9
  
  
--- tujian.py
+++ tujian.py
@@ -2,8 +2,9 @@ import base64
 import json
 import requests
 import time
+import os
 img_path="qrcode_temp.png"
-def base64_api(uname='Zion', pwd='123456qwerty', img=img_path):
+def base64_api(uname, pwd, img=img_path):
     with open(img, 'rb') as f:
         base64_data = base64.b64encode(f.read())
         b64 = base64_data.decode()
@@ -16,5 +17,8 @@ def base64_api(uname='Zion', pwd='123456qwerty', img=img_path):
 
 
 if __name__ == "__main__":
+    uname = os.environ["UNAME"]
+    pwd=os.environ["PWD"]
+
     result = base64_api()
     print(result)
